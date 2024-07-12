807affa56895f3f3158337242ef631a1c8f5e6ea
Atharv-28 <atharvmt@outlook.com>
2024-07-13 00:57:21 +0530

    Intro done

--- src/components/intro.js
+++ src/components/intro.js
@@ -5,14 +5,15 @@ import "../styles/intro.css"
 const Intro = () => {
   return (
     <div id='intro' className='intro'>
-        <div>
+        <div className='intro-image'>
+            <img className='img-prof' src={introImg} />
+        </div>
+        <div  className='intro-text'>
             <p>Hi,</p>
             <h2>I'm Atharv Tambekar</h2>
             <h3>Front-End Developer</h3>
         </div>
-        <div>
-            <img src={introImg} />
-        </div>
+        
     </div>
   );
 }

dcc120ec01dd8959341dc7fb7cec3587a7dd420f
Atharv-28 <atharvmt@outlook.com>
2025-01-18 20:54:33 +0530

    reveal & animation to project card added

--- src/page/homePage.js
+++ src/page/homePage.js
@@ -3,17 +3,28 @@ import Career from "../components/career";
 import Intro from "../components/intro";
 import AboutMe from "../components/aboutMe";
 import Skills from "../components/skills";
-import "../App.css";
 import Footer from "../components/footer";
+import Fade from "react-reveal/Fade";
+import "../App.css";
 
 const HomePage = () => {
   return (
     <div className="home">
-      <Intro />
-      <AboutMe />
-      <Career />
-      <Skills />
-      <Footer />
+      <Fade bottom>
+        <Intro />
+      </Fade>
+      <Fade bottom>
+        <AboutMe />
+      </Fade>
+      <Fade bottom>
+        <Career />
+      </Fade>
+      <Fade bottom>
+        <Skills />
+      </Fade>
+      <Fade bottom>
+        <Footer />
+      </Fade>
     </div>
   );
 };

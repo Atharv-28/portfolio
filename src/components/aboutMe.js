14931d911acbe733faef2a71462bec80e5f6aff0
Atharv-28 <atharvmt@outlook.com>
2025-01-23 20:21:48 +0530

    skills animation improved

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -7,7 +7,7 @@ import PersonIcon from "@mui/icons-material/Person";
 const AboutMe = () => {
   return (
     <div className="about-me-container">
-      <h2 className="about-me-title">About me</h2>
+      <h2 className="about-me-title">About Me</h2>
       <div className="about-me-content">
         <div className="about-me-left">
           <p className="about-me-subtitle">

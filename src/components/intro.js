359f053543939bd58787d07171b1a4875053c87f
Atharv-28 <atharvmt@outlook.com>
2025-02-15 18:00:01 +0530

    React bits components started to add

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React from "react";
 import introImg from "./prof.png";
-import Contact from './contact';
+import Contact from "./contact";
+import BlurText from "./BlurText";
+
 import "../styles/intro.css";
 
 const Intro = () => {
+
+  const handleAnimationComplete = () => {
+    console.log('Animation completed!');
+  };
+
   return (
-    <div id='intro' className='intro'>
-        <div className='intro-image'>
-            <img className='img-prof' src={introImg} />
-        </div>
-        <div  className='intro-text'>
-            <p>Hi,</p>
-            <h2>I'm Atharv Tambekar</h2>
-            <h3>M-E-R-N Stack Developer | Mobile App Developer (React-Native)</h3>
-            <Contact />
-        </div>
+    <div id="intro" className="intro">
+      <div className="intro-image">
+        <img className="img-prof" src={introImg} />
+      </div>
+      <div className="intro-text">
+        <p>Hi,</p>
+        <BlurText
+          text="I'm Atharv Tambekar"
+          delay={450}
+          animateBy="words"
+          direction="top"
+          onAnimationComplete={handleAnimationComplete}
+          className="text-5xl mb-8"
+        />
+        <h2></h2>
+        <h3>M-E-R-N Stack Developer | Mobile App Developer (React-Native)</h3>
+        <Contact />
+      </div>
     </div>
   );
-}
+};
 
 export default Intro;
-
-

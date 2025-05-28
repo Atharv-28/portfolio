9186a7c123d8192923d220f7d1480540a1d753ee
Atharv-28 <atharvmt@outlook.com>
2025-05-28 23:11:12 +0530

    intro- skills animated

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,8 +1,8 @@
 import React from "react";
 import introImg from "./prof.png";
-import Contact from "./contact";
 import BlurText from "./BlurText";
 import SplitText from "./SplitText";
+import RotatingText from "./RotatingText";
 
 import "../styles/intro.css";
 
@@ -35,7 +35,27 @@ const Intro = () => {
           direction="top"
           onAnimationComplete={handleAnimationComplete}
         />
-        <h3>M-E-R-N Stack Developer | Mobile App Developer (React-Native)</h3>
+        {/* <h3>M-E-R-N Stack Developer | Mobile App Developer (React-Native)</h3> */}
+        <div className="div-rotating-text">
+          <RotatingText
+            texts={[
+              "M-E-R-N Stack Developer",
+              "Mobile App Developer (React-Native)",
+              "Computer Science Engineer",
+              "Software Developer",
+            ]}
+            mainClassName="px-2 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+            staggerFrom={"last"}
+            initial={{ y: "100%" }}
+            animate={{ y: 0 }}
+            className=" skill-text"
+            exit={{ y: "-120%" }}
+            staggerDuration={0.025}
+            splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+            transition={{ type: "spring", damping: 30, stiffness: 400 }}
+            rotationInterval={5000}
+          />
+        </div>
       </div>
     </div>
   );

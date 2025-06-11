caf6ad012245c281b166eaf7f5f481bb4a2344cc
Atharv-28 <atharvmt@outlook.com>
2025-06-11 15:34:17 +0530

    breadcrumb added to project page

--- src/page/projectDetails.js
+++ src/page/projectDetails.js
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
-import { useParams } from 'react-router-dom';
-import ProjectList from '../utils/projectList';
-import GitHubIcon from '@mui/icons-material/GitHub';
-import OpenInNewIcon from '@mui/icons-material/OpenInNew'; 
+import React, { useState } from "react";
+import { useParams } from "react-router-dom";
+import ProjectList from "../utils/projectList";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 import "../styles/projectDetails.css";
 
 const ProjectDetails = () => {
   const { projectName } = useParams();
 
-  const project = ProjectList.find(proj => proj.projectName === projectName);
+  const project = ProjectList.find((proj) => proj.projectName === projectName);
 
   const [currentSlide, setCurrentSlide] = useState(0);
 
@@ -21,24 +21,43 @@ const ProjectDetails = () => {
   };
 
   const prevSlide = () => {
-    setCurrentSlide((prevSlide) => (prevSlide - 1 + project.snapshots.length) % project.snapshots.length);
+    setCurrentSlide(
+      (prevSlide) =>
+        (prevSlide - 1 + project.snapshots.length) % project.snapshots.length
+    );
   };
 
   return (
     <div className="project-details-container">
-      <div className='project-details-heading'>
-      <img className='project-ico' src={project.icon} alt={`${project.projectName} Icon`} />
-      <h2>{project.projectName}</h2>
+      <div className="project-details-breadcrumbs">
+        <a href="/">Projects / </a>
+        <span>Project Details</span>
+      </div>
+      <div className="project-details-heading">
+        <img
+          className="project-ico"
+          src={project.icon}
+          alt={`${project.projectName} Icon`}
+        />
+        <h2>{project.projectName}</h2>
       </div>
       <div className="project-description">
         <p>{project.description}</p>
-        <p className='tech-stack'>Tech Stack used: {project.info}</p>
+        <p className="tech-stack">Tech Stack used: {project.info}</p>
         <h3>Snapshots</h3>
         {project.snapshots.length > 0 ? (
           <div className="slideshow-container">
-            <button className="prev" onClick={prevSlide}>&#10094;</button>
-            <img src={project.snapshots[currentSlide]} alt={`Snapshot ${currentSlide + 1}`} className="snapshot-image" />
-            <button className="next" onClick={nextSlide}>&#10095;</button>
+            <button className="prev" onClick={prevSlide}>
+              &#10094;
+            </button>
+            <img
+              src={project.snapshots[currentSlide]}
+              alt={`Snapshot ${currentSlide + 1}`}
+              className="snapshot-image"
+            />
+            <button className="next" onClick={nextSlide}>
+              &#10095;
+            </button>
           </div>
         ) : (
           <p>No snapshots available.</p>
